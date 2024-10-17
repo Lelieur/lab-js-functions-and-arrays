737194a10670d0b4a79520cc77e548b05d4a5663
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-10-17 18:19:37 +0200

    Everything resolved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+
+    if (number1 > number2) {
+        return number1
+    } else if (number2 > number1) {
+        return number2
+    } else {
+        return number1
+    }
+
+}
 
 
 
@@ -7,7 +17,28 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(myArray) {
+
+    let longestWord = ""
+
+
+    if (myArray.length === 0) {
+        return null
+
+    }
+
+    for (let i = 0; i < myArray.length; i++) {
+
+        if (longestWord.length < myArray[i].length) {
+
+            longestWord = myArray[i];
+        }
+
+    }
+
+    return longestWord
+}
+
 
 
 
@@ -15,7 +46,19 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(myArray) {
+
+    let totalSum = 0;
+
+    for (let i = 0; i < myArray.length; i++) {
+
+        totalSum += myArray[i]
+
+    }
+
+    return totalSum
+
+}
 
 
 
@@ -23,7 +66,25 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(myArray) {
+
+    let totalSum = 0;
+
+    if (myArray.length === 0) {
+        return 0;
+
+    } else {
+
+        for (let i = 0; i < myArray.length; i++) {
+
+            totalSum += myArray[i]
+
+        }
+
+    }
+
+    return (totalSum / myArray.length)
+}
 
 
 
@@ -31,4 +92,22 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(myArray, myWord) {
+
+    let wordFound;
+
+    if (myArray.length === 0) {
+        wordFound = null;
+    } else {
+        if (myArray.indexOf(myWord) > 0) {
+            wordFound = true
+        } else {
+            wordFound = false
+        }
+
+    }
+
+
+    return wordFound
+
+}
